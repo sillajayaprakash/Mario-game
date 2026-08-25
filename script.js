d3f1cf4a084beb38aae881a598fbf61ac8db8ded
sillajayaprakash <sillajayapraksh0206@gmail.com>
2026-08-25 06:28:27 +0530

    Add Mario right movement

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ let mario = document.querySelector(".mario");
 let marioX = 50;
 
 console.log("Mario game started");
+
+let mario = document.querySelector(".mario");
+
+let marioX = 50;
+
+document.addEventListener("keydown", function (e) {
+
+  if (e.key === "d" || e.key === "ArrowRight") {
+
+    marioX = marioX + 10;
+
+    if (marioX > 750) {
+      marioX = 750;
+    }
+
+    mario.style.left = marioX + "px";
+  }
+
+});
